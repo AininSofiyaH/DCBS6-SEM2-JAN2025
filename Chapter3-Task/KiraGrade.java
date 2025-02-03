f4c0a67db3b7a0228ea5d72acf6af6a2f46fb161
khairul_irfan <khairul.irfan3107@gmail.com>
2025-02-03 09:18:39 +0800

    Add files via upload

---
+++ Chapter3-Task/KiraGrade.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class KiraGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai akhir: ");
+        int mark = scanner.nextInt();
+        String grade;
+
+        if (mark >= 90) {
+            grade = "A+";
+        } else if (mark >= 80) {
+            grade = "A";
+        } else if (mark >= 70) {
+            grade = "A-";
+        } else if (mark >= 65) {
+            grade = "B+";
+        } else if (mark >= 60) {
+            grade = "B";
+        } else if (mark >= 55) {
+            grade = "C+";
+        } else if (mark >= 50) {
+            grade = "C";
+        } else if (mark >= 45) {
+            grade = "D";
+        } else if (mark >= 40) {
+            grade = "E";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("Grade: " + grade);
+        scanner.close();
+    }
+}
