6db7e18b1fc96f12fbc5c5bdc73cd750fe3a903a
Khair <a_khair2@hotmail.com>
2025-02-10 12:17:54 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_2/CHAPTER4_TASK2_KHAIR.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classspg;
+
+/**
+ *
+ * @author a_kha
+ */
+ public static void main(String[] args) {
+    
+    System.out.println("Number 1 to 10 using for structure");
+    for (int count = 1; count <= 10; count++){
+        System.out.println(count); }
+    
+   
+    System.out.println();
+    
+    int count = 1;
+        System.out.println("Number 1 to 10 using while structure");
+            while (count <= 10) {
+                System.out.println(count++);
+            }
+            
+    int Count = 1;
+        System.out.println("Number 1 to 10 using do while sttucture");
+            do{
+                System.out.println(Count++);
+            }
+            while (Count <= 10);
+    
+    
+
+}
+    
+  
+
+    
+
+
+
