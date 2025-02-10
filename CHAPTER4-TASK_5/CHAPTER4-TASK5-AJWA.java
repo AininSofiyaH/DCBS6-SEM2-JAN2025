0fcce764ebe4fe72d7c65f9d650e67ff91fa7450
qasrienaajwa <qasrienaajwa@gmail.com>
2025-02-10 12:10:19 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-AJWA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter4task5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Chapter4Task5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Displaying the sequence: 99 88 77 66 55 44 33 22 11\n");
+        
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+    }
+}
+
+ 
