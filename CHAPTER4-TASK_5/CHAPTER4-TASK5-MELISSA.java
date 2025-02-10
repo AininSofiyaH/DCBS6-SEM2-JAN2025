ad24d06c1e351e2b519576377fecbe2759ae5c2e
Melissa <melissaamel771@gmail.com>
2025-02-10 12:37:44 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-MELISSA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter4_task5;
+
+/**
+ *
+ * @author PC STUDENT 04
+ */
+public class Chapter4_task5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 99; i>= 11; i -=11){
+        System.out.println (i); }
+    }
+    
+}
