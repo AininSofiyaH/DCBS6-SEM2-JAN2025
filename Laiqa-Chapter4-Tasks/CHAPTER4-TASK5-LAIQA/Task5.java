b68e66751e8ff9497b0762f5ed772dd5b9dbb8d2
Laiqa <laiqanr213@gmail.com>
2025-02-10 11:39:59 +0800

    Delete Laiqa-Chapter4-Tasks directory

--- Laiqa-Chapter4-Tasks/CHAPTER4-TASK5-LAIQA/Task5.java
+++
@@ -1,8 +0,0 @@
-package task5;
-public class Task5 {
-    public static void main(String[] args) {
-         for (int i = 99; i >= 11; i -= 11) {
-            System.out.println(i);}
-    }
-    
-}
