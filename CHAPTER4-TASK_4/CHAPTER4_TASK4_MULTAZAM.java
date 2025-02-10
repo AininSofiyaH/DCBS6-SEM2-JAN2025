0e6e08b3c384cb9f786ef662a78a5a6fca644781
zammmmmm433 <muhdmultazam342@gmail.com>
2025-02-10 12:30:00 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_4/CHAPTER4_TASK4_MULTAZAM.java
@@ -0,0 +1,11 @@
+public class CHAPTER4_TASK4_MULTAZAM{
+    public static void main(String[] args) {
+        int sum = 0;
+        
+        for (int a = 1; a <= 10; a++) {
+            sum = sum + (a * a);
+        }
+        
+        System.out.println("Sum of squares from 1-10: " + sum);
+    }
+}
