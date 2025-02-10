f5e71155206e4203b463602960f259a52de3b38a
anipjer <anipjer123@gmail.com>
2025-02-10 11:58:40 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-MUHAMMAD_HANIF.java
@@ -0,0 +1,8 @@
+class ReverseSequence {
+    public static void main(String[] args) {
+        System.out.println("Sequence: ");
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+    }
+}
