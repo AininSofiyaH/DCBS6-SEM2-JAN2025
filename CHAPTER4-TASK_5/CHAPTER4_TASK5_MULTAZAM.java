5f853673007a916719f7f501b6501beff37f0903
zammmmmm433 <muhdmultazam342@gmail.com>
2025-02-10 12:36:55 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4_TASK5_MULTAZAM.java
@@ -0,0 +1,7 @@
+public class CHAPTER4_TASK5_MULTAZAM {
+    public static void main(String[] args) {
+        for (int a = 99; a >= 11; a = a - 11) {
+            System.out.print(a + " ");
+        }
+    }
+}
