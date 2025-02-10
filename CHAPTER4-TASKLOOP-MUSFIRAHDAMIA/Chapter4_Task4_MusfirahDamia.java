1b0fa626ddbe739e13611826a8d614dfabc3a8f0
Musfirah damia <gartuutiyou@gmail.com>
2025-02-10 11:34:36 +0800

    Add files via upload

---
+++ CHAPTER4-TASKLOOP-MUSFIRAHDAMIA/Chapter4_Task4_MusfirahDamia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chap4_task1;
+
+/**
+ *
+ * @author Damia
+ */
+public class Chapter4_Task4_MusfirahDamia {
+     public static void main(String[] args) {
+    int countFor;
+    int result=0,sumpow=0;
+    for(countFor =1; countFor<=10;countFor++){
+        
+        result=countFor*countFor;
+        System.out.println(countFor+"^"+countFor+"="+result);
+        sumpow +=result;
+                }
+    System.out.println("sum is"+sumpow);
+            
+           
+                
+                
+        }
+    
+}
