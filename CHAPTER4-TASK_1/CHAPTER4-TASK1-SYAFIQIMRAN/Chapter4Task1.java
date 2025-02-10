4c2d2cbb6d0365c4f786730a3ae5777d0a0632ca
Syafiqimran <shadowcreeper006@gmail.com>
2025-02-10 12:18:03 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_1/CHAPTER4-TASK1-SYAFIQIMRAN/Chapter4Task1.java
@@ -0,0 +1,22 @@
+public class Chapter4Task1 {
+    
+   public static void main(String[] args){
+       System.out.println("Printing Numbers from 1 to 10");
+       for (int count = 1; count <= 10; count++) {
+           System.out.println(count);
+       }
+int count2= 1;
+       System.out.println("Printing number 1 to 10");
+       while (count2 <= 10){
+       System.out.println(count2++);}
+       
+
+int count3 = 1;
+       System.out.println("Printing number 1 to 10");
+       do{
+           System.out.println(count3++);}
+       while(count3<=10);
+       }
+}
+  
+
