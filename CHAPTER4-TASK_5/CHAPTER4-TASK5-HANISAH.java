b78da382498d1ce2dfc205295eb0c98e74c4e826
hanisah <hanisahanuar06@gmail.com>
2025-02-10 12:47:13 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-HANISAH.java
@@ -0,0 +1,15 @@
+package chap4task5;
+
+public class Chap4task5 {
+
+    public static void main(String[] args) {
+        System.out.println("Displaying the sequence: 99 88 77 66 55 44 33 22 11\n");
+        
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+    }
+    
+}
