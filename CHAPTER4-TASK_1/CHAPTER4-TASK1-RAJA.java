d81f16c5d9c05900f1b4052aeb6e3f09fb224492
Ajepudin <rajasyarafuddin@gmail.com>
2025-02-10 11:53:22 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_1/CHAPTER4-TASK1-RAJA.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("=================");
+		System.out.println(" Task for While ");
+		System.out.println("=================");
+		int count = 1;
+		System.out.println("Printing Numbers from 1 to 10");
+		while(count <= 10) {
+			System.out.println(count++);
+		}
+		
+		
+		System.out.println("\n=================");
+		System.out.println("Task for Do While");
+		System.out.println("=================");
+		int count1 = 1;
+		System.out.println("Printing Numbers from 1 to 10");
+		do {
+			System.out.println(count1++);}
+		while(count1 <= 10); 
+					
+		
+		
+		System.out.println("\n=================");
+		System.out.println("  Task for For");
+		System.out.println("=================");
+		System.out.println("Printing Numbers from 1 to 10");
+		for(int count2 = 1;count2 <= 10;count2++) {
+			System.out.println(count2);}
+		
+		
+
+	}
+
+}
