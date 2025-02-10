d25a52d1506cf92c00f6812c720b08798539d1b8
Khair <a_khair2@hotmail.com>
2025-02-10 12:34:58 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/Chapter4Task5_Ahmad_Khair.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classspg;
+
+/**
+ *
+ * @author a_kha
+ */
+public class Chapter4Task5_Ahmad_Khair {
+        public static void main(String[] args) {
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+    
+        }}}
