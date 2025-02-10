49fa6a817018dfd27a24c29e654872945cb18f85
zammmmmm433 <muhdmultazam342@gmail.com>
2025-02-10 12:20:29 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_3/Chapter3_Task3_Multazam.java
@@ -0,0 +1,15 @@
+public class Chapter3_Task3_Multazam {
+    public static void main(String[] args) {
+        int Even = 0;
+        int Odd = 0;
+
+        for (int a = 2; a <= 10; a = a + 2) 
+            Even = Even + a;
+        for (int a = 1; a <= 9; a = a + 2) 
+            Odd = Odd + a;
+
+        System.out.println("Sum of even numbers: " + Even);
+        System.out.println("Sum of odd numbers: " + Odd);
+    }
+}
+
