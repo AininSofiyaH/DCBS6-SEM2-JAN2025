6a99d03d50b0f1a12edfafd1a061bf2ad58d72e5
syazmin <iskandarsyazmin@gmail.com>
2025-02-10 12:05:04 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-SYAZMIN.java
@@ -0,0 +1,9 @@
+class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Number Sequence:");
+
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+    }
+}
