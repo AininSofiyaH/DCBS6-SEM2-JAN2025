239bf0d34134f6d94644d6b7e9c703fc693ee6bc
khairul_irfan <khairul.irfan3107@gmail.com>
2025-02-10 12:01:45 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_3/Task3_Khairul_Irfan.java
@@ -0,0 +1,22 @@
+public class Task3_Khairul_Irfan {
+    public static void main(String[] args) {
+        int evenSum = 0, oddSum = 0;
+        System.out.println("Even numbers: ");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+                evenSum += i;
+            }
+        }
+        System.out.println("\nSum of even numbers: " + evenSum);
+
+        System.out.println("\nOdd numbers: ");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+                oddSum += i;
+            }
+        }
+        System.out.println("\nSum of odd numbers: " + oddSum);
+    }
+}
