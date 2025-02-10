b9fd6387630631df3c13c320226e872014bb5660
amirrhans <amirhaziq0402@gmail.com>
2025-02-10 12:38:19 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_1/Chapter4Task1-amirhaziq.java
@@ -0,0 +1,29 @@
+
+public class Chapter4Task1 {
+    public static void main(String[] args){
+        System.out.println("Printing numbers from 1 to 10");
+        for (int count = 1;count <= 10; count++) {
+            System.out.println(count);
+        }  
+           
+
+
+//Loop while
+int count2 = 1;
+       System.out.println("Printing numbers from 1 to 10");
+       while(count2 <=10){
+       System.out.println(count2++);}
+       
+//loop do while
+int count3 = 1;
+       System.out.println("Printing numbers from 1 to 10");
+       do{
+           System.out.println(count3++);}
+       while(count3<=10);
+       }
+    }
+
+       
+       
+
+
