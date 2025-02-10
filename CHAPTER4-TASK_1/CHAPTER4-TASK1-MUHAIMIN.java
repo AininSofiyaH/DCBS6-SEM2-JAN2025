217c47b97c8962589e85aae405585191b4ae4c04
meen <muhaiminmahadi4@gmail.com>
2025-02-10 11:46:51 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_1/CHAPTER4-TASK1-MUHAIMIN.java
@@ -0,0 +1,30 @@
+public class Int_1_to_10 {
+//////MUHAIMIN BIN MAHADI
+    public static void main(String[] args) {
+        
+        //WHILE LOOP
+        int count1 = 1; 
+	    System.out.println("Printing Numbers from 1 to 10"); 
+	    while (count1 <= 10) { 
+                	System.out.println(count1++);} 
+                        System.out.println(""); 
+            
+        // DO-WHILE LOOP   
+        int count2 = 1; 
+	    System.out.println("Printing Numbers from 1 to 10"); 
+	    do {
+ 	          System.out.println(count2++); }
+	    while (count2 <= 10);
+            System.out.println(""); 
+
+        
+        //FOR Loop
+        System.out.println("Printing Numbers from 1 to 10");
+        for (int count3 = 1; count3 <= 10; count3++) {
+            System.out.println(count3);}
+            System.out.println("");
+            
+
+    }
+    
+}
