278e045dc80b4a74105c96ad36fbc177c6c48ef2
aiena <aienanuwaira.rosli@student.gmi.edu.my>
2025-02-10 12:31:46 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-AIENA.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter4.task.pkg5;
+
+/**
+ *
+ * @author Nuwaira
+ */
+public class Chapter4Task5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + "  ");
+        }
+    }
+}
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+
