c6d37a0d18178ba1baa99025969dac2009831a89
syazmin <iskandarsyazmin@gmail.com>
2025-02-10 12:03:43 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_3/CHAPTER4-TASK3-SYAZMIN.java
@@ -0,0 +1,23 @@
+class Task3 {
+    public static void main(String[] args) {
+        int evenSum = 0, oddSum = 0;
+
+        System.out.println("Even numbers between 1 and 10:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+                evenSum += i;
+            }
+        }
+        System.out.println("\nSum of even numbers = " + evenSum);
+
+        System.out.println("\nOdd numbers between 1 and 10:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+                oddSum += i;
+            }
+        }
+        System.out.println("\nSum of odd numbers = " + oddSum);
+    }
+}
