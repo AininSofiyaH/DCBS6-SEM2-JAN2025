77d8d0f4e4190d48c2328ef0910896534b2b0b50
aiena <aienanuwaira.rosli@student.gmi.edu.my>
2025-02-10 11:39:10 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_1/CHAPTER4-TASK1-AIENA_NUWAIRA.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chapter.pkg4.task.pkg1;
+
+/**
+ *
+ * @author Nuwaira
+ */
+public class Chapter4Task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("using for loop:");
+            for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+        
+        // while loop
+        System.out.println("using while loop:");
+        int a = 1;
+            while (a <= 10) {
+        System.out.print(a + " ");
+        a++;
+        }
+        System.out.println("\n");
+        
+        // do-while loop
+        System.out.println("using do-while loop:");
+        int b = 1;
+            do {
+        System.out.print(b + " ");
+            b++;
+        } 
+           while (b <= 10);
+        System.out.println();
+    }}
+
+
+
+
+
+
+
+
+
+    
+    
+
