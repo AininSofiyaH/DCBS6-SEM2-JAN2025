5a61cc98bc5cd6e702da26a452bec341e80deac9
Amir Arshad <amirarshad06@gmail.com>
2025-02-10 12:24:08 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_4/CHAPTER4-TASK4-ARSHAD.java
@@ -0,0 +1,9 @@
+public class task4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            System.out.println("square of " + i + " is: " + square);
+        }
+    }
+}
