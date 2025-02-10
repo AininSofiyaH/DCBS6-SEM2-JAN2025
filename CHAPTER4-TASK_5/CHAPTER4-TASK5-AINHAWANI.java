44c19ffd3f1a40d0a8cb8e68813677d5b67d031a
ain <hawaniain24@gmail.com>
2025-02-10 12:48:09 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-AINHAWANI.java
@@ -0,0 +1,12 @@
+
+package loopingtask5;
+
+
+public class LoopingTask5 {
+    public static void main(String[] args) {
+        for (int i = 99; i>=11; i -=11) {
+            System.out.println(i);}
+        }
+    }
+    
+
