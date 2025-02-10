a79212908a32aeb8e87b9033e7fb06e316e46cda
amirrhans <amirhaziq0402@gmail.com>
2025-02-10 12:43:11 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/Chapter4Task5-amirhaziq.java
@@ -0,0 +1,8 @@
+
+public class Chapter4Task5 {
+    public static void main(String[] args) {        
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+    }
+}
