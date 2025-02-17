f04a604dab8bea41d8c358a4364beed43f6763cc
Akmal <akmalmus2006@gmail.com>
2025-02-17 10:31:44 +0800

    Update CHAPTER4-TWO-TASK1-MUSTOFA.java

--- CHAPTER4-TWO-TASK_1/CHAPTER4-TWO-TASK1-MUSTOFA.java
+++ CHAPTER4-TWO-TASK_1/CHAPTER4-TWO-TASK1-MUSTOFA.java
@@ -3,17 +3,17 @@ import javax.swing.JOptionPane;
 public class Password {
     public static void main(String[] args) {
 
-        String firstGUI;
+        String firstGUI; 
         String password = "ipin";
 
-        for (int i = 3; i >= 0; i--) {
+        for (int i = 3; i >= 0; i--) { //3 times loops for 3 attempts only
             firstGUI = JOptionPane.showInputDialog("Enter your password:");
-            if (!firstGUI.equals(password)) {
+            if (!firstGUI.equals(password)) { //if password is wrong
             JOptionPane.showMessageDialog(null, "Wrong password. You have " + i + " more attempt. Hint: budak botak", "Error", JOptionPane.PLAIN_MESSAGE);
-        if (i == 0)
+        if (i == 0) //if the user out of attempts
             JOptionPane.showMessageDialog(null, "Too many attempts. Please try again in 30 minutes ", "Error", JOptionPane.PLAIN_MESSAGE);
             }
-        else {
+        else { //if password is correct it will break the loop
             JOptionPane.showMessageDialog(null, "Welcome user", "Success", JOptionPane.PLAIN_MESSAGE);
             break;
             }
