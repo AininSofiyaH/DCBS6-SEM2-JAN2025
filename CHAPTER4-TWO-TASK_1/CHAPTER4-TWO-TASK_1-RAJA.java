94afa16f4004c77f0b415553c33925087c9e0835
Ajepudin <rajasyarafuddin@gmail.com>
2025-02-17 09:47:49 +0800

    Add files via upload

---
+++ CHAPTER4-TWO-TASK_1/CHAPTER4-TWO-TASK_1-RAJA.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Jujukan Nombor:");
+
+        for (int i = 99; i >= 11; i -= 11) { 
+            System.out.print(i + " ");
+        }
+    }
+}
