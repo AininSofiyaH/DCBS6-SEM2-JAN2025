27e91d312e4b6e3f4256aa88aeb59de25dec97fd
Akmal <akmalmus2006@gmail.com>
2025-02-17 08:06:18 +0800

    Add files via upload

---
+++ CHAPTER4-TASK_5/CHAPTER4-TASK5-MUSTOFA.java
@@ -0,0 +1,9 @@
+public class Number_Sequence {
+    public static void main(String[] args) {
+
+        System.out.println("Decreasing Number Sequence:");
+        for (int num = 99; num >= 1; num -= 11){
+            System.out.println(num);
+        }
+    }
+}
