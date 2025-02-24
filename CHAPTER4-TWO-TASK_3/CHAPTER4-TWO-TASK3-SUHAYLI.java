7d6ce9c388ad5a324826682013e30de3b35783ce
lam <ilhmsshyli@gmail.com>
2025-02-24 10:47:30 +0800

    Update CHAPTER4-TWO-TASK3-SUHAYLI.java

--- CHAPTER4-TWO-TASK_3/CHAPTER4-TWO-TASK3-SUHAYLI.java
+++ CHAPTER4-TWO-TASK_3/CHAPTER4-TWO-TASK3-SUHAYLI.java
@@ -15,14 +15,15 @@ public class SaleLetter {
         Scanner read = new Scanner(System.in);
 
         final double BASE_SALARY = 2600.00; //salary
-        final double COMMISSION_RATE = 0.05; 
+        final double COMMISSION_RATE = 0.05; //commission rate is 5%
 
         System.out.print("Enter the total sales for the week: RM");
-        double grossSales = read.nextDouble();
+        double grossSales = read.nextDouble(); //read user's input for gross sale
 
         double commission = grossSales * COMMISSION_RATE; 
         double totalEarnings = BASE_SALARY + commission; 
 
+        //earning's breakdown
         System.out.println("Weekly earnings breakdown:");
         System.out.println("Base salary: RM" + BASE_SALARY);
         System.out.println("Commission (5% of RM" + grossSales + "): RM" + commission);
