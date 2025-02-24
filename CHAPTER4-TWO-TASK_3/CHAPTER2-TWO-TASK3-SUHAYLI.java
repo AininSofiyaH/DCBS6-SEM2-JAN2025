3309f673ae8d1577e190020506ddacf3b4608142
lam <ilhmsshyli@gmail.com>
2025-02-24 08:06:33 +0800

    Add files via upload

---
+++ CHAPTER4-TWO-TASK_3/CHAPTER2-TWO-TASK3-SUHAYLI.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lammm
+ */
+import java.util.Scanner;
+
+public class SaleLetter {
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        final double BASE_SALARY = 2600.00; //salary
+        final double COMMISSION_RATE = 0.05; 
+
+        System.out.print("Enter the total sales for the week: RM");
+        double grossSales = read.nextDouble();
+
+        double commission = grossSales * COMMISSION_RATE; // calculating commission
+
+        double totalEarnings = BASE_SALARY + commission; // calculate total earning
+
+        System.out.println("Weekly earnings breakdown:");
+        System.out.println("Base salary: RM" + BASE_SALARY);
+        System.out.println("Commission (5% of RM" + grossSales + "): RM" + commission);
+        System.out.println("Total earnings for the week: RM" + totalEarnings);
+
+        read.close();
+    }
+}
+    
+
