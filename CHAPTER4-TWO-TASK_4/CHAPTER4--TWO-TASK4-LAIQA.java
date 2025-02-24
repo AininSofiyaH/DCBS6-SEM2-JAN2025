1093e1c03a934227cd530e188fb600dd2ae80dbd
Laiqa <laiqanr213@gmail.com>
2025-02-24 20:43:24 +0800

    Update CHAPTER4--TWO-TASK4-LAIQA.java

--- CHAPTER4-TWO-TASK_4/CHAPTER4--TWO-TASK4-LAIQA.java
+++ CHAPTER4-TWO-TASK_4/CHAPTER4--TWO-TASK4-LAIQA.java
@@ -7,18 +7,18 @@ public class EvenOddNum {
    
        do {
         System.out.print("Enter the number of integers in the sequence: ");
-        num = numinput.nextInt();
+        num = numinput.nextInt(); //for user to input bilangan nombor
         
         even = 0;
         odd = 0;
         
         for (int i = 0; i < num; i++) {
             System.out.print("Enter integer " + (i + 1) + ": ");
-            number = numinput.nextInt();
+            number = numinput.nextInt(); // to input each number
             
-            if (number % 2 == 0) {
-                even++;
-            } else {
+            if (number % 2 == 0) { //nombor tu even kalau remainder dia 0 after bahagi dgn 2
+                even++; 
+            } else { // kalau tak odd lah
                 odd++;
             }
         }
@@ -26,11 +26,11 @@ public class EvenOddNum {
         System.out.println("Number of even integers: " + even);
         System.out.println("Number of odd integers: " + odd);
         
-        System.out.print("Do you want to continue? (1 = yes, 0 = exit)): ");
+        System.out.print("Do you want to continue? (1 = yes, 0 = exit)): "); // let user decide if they want to continue or exit
         choice = numinput.nextInt();
-       } while (choice == 1);
+       } while (choice == 1); //loop - the system will run again if user input 1
            
-            System.out.println("Goodbye!");
+            System.out.println("Goodbye!"); 
              numinput.close();
        } 
    
