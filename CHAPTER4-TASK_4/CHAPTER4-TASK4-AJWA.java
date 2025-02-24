03e23b66eeb7d63d5d17f5d42b653147ca287a82
qasrienaajwa <qasrienaajwa@gmail.com>
2025-02-24 14:59:25 +0800

    Update CHAPTER4-TASK4-AJWA.java

--- CHAPTER4-TASK_4/CHAPTER4-TASK4-AJWA.java
+++ CHAPTER4-TASK_4/CHAPTER4-TASK4-AJWA.java
@@ -14,16 +14,16 @@ public class Chapter4Task4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) { 
-      int sum = 0;
+      int sum = 0; // Variable to store the sum of squares
 
         System.out.println("Calculating the sum of squares (n x n) from 1 to 10:\n");
-
+         // Loop through numbers from 1 to 10
         for (int i = 1; i <= 10; i++) {
-            int square = i * i;
+            int square = i * i; // Calculate the square of the number
             System.out.println(i + "^2 " + i + " = " + square);
-            sum += square;
+            sum += square; // Add the square to the total sum
         }
-
+// Display the total sum
         System.out.println("\nThe total sum of squares from 1 to 10 is: " + sum);
     }
 }
