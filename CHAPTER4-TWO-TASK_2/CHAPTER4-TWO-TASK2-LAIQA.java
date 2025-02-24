d187d8c956ff4920a57c814bb61411c9be7c9555
Laiqa <laiqanr213@gmail.com>
2025-02-24 10:49:38 +0800

    Update CHAPTER4-TWO-TASK2-LAIQA.java

--- CHAPTER4-TWO-TASK_2/CHAPTER4-TWO-TASK2-LAIQA.java
+++ CHAPTER4-TWO-TASK_2/CHAPTER4-TWO-TASK2-LAIQA.java
@@ -6,13 +6,13 @@ public class Testresults {
      { Scanner inp = new Scanner(System.in);
 
   System.out.print("Enter the number of students: ");
-    numstu = inp.nextInt();
+    numstu = inp.nextInt(); //scanner utk tahu bilangan student
     
-    for (int a = 0; a < numstu; a++){
+    for (int a = 0; a < numstu; a++){ //selagi tak capai bil. student, dia akan bertambah
         System.out.print("Enter result for student " + (a + 1) + " (1 for pass, 2 for fail): ");
         result = inp.nextInt();
         
-        if (result == 1) {
+        if (result == 1) { 
             pass++;}
             
         else {
@@ -24,7 +24,7 @@ public class Testresults {
     System.out.println("Number of students passed: " + pass);
     System.out.println("Number of students failed: " + fail);
         
-    if (pass > numstu / 2) {
+    if (pass > numstu / 2) { //kalau yang pass lebih 50%
         System.out.println("##Bonus to instructor##");
     }
      inp.close();
