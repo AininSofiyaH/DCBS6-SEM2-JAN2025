214f96f887d840a6a5f951cb29d2e725e1479b67
Laiqa <laiqanr213@gmail.com>
2025-03-12 13:59:50 +0800

    Update Unitconversion.java

--- Chapter 5 TASKS - Laiqa/TASK 4 - CHAPTER5/Unitconversion.java
+++ Chapter 5 TASKS - Laiqa/TASK 4 - CHAPTER5/Unitconversion.java
@@ -4,11 +4,13 @@ public class Unitconversion {
 
     public static void main(String[] args) {
         Scanner userinp = new Scanner(System.in);
+        //declare and initialize
         int choice;
-        double value, result = 0;
-        formula objFormula = new formula();
+        double value, result = 0; 
+        formula objFormula = new formula(); //call method for formula class
         
         do {
+            //display menu
             System.out.println("Unit Conversion Menu:");
             System.out.println("1. Temperature (Celsius to Fahrenheit)");
             System.out.println("2. Length (Meters to Feet)");
@@ -17,18 +19,18 @@ public class Unitconversion {
             System.out.print("Choose an option (1-4): ");
             choice = userinp.nextInt();
             
-              if (choice == 4) {
+              if (choice == 4) { //exit when user input 4
                 System.out.println("### Program exited. Thank you for using this system. ###");
                 break;}
                 
-            System.out.print("Enter value to convert: ");
+            System.out.print("Enter value to convert: "); //let user input value to convert
             value = userinp.nextDouble();
             
             
             switch (choice) {
                 case 1:
-                    result = objFormula.temp(value);
-                    System.out.println(value + " celcius is " + result + " fahrenheit.\n");
+                    result = objFormula.temp(value); //call method from class formula
+                    System.out.println(value + " celcius is " + result + " fahrenheit.\n"); //display
                     break;
                     
                 case 2:
@@ -42,10 +44,10 @@ public class Unitconversion {
                     break;
                     
                 default:
-                    System.out.println("### Invalid choice. Please try again. ###");
+                    System.out.println("### Invalid choice. Please try again. ###"); //error message if user input anything other than 1-4
             }
 
-        } while (choice!=4);
+        } while (choice!=4); //loop selagi user tak input 4 
                 
     }
 }
