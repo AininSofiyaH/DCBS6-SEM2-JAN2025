f69423f476ebd336b23b74e73629ecf18bf66876
Laiqa <laiqanr213@gmail.com>
2025-03-12 13:55:07 +0800

    Update Arithmeticchap5.java

--- Chapter 5 TASKS - Laiqa/TASK 2 - CHAPTER5/Arithmeticchap5.java
+++ Chapter 5 TASKS - Laiqa/TASK 2 - CHAPTER5/Arithmeticchap5.java
@@ -13,13 +13,15 @@ public class Arithmeticchap5 {
         System.out.println("Enter 2nd Integer: ");
         num2 = numinput.nextInt();
         numinput.close();
-        
+
+        //call methods for formulas
         sum = add(num1,num2);
         sub = minus(num1,num2);
         mul = darab(num1,num2);
         div = bahagi(num1,num2);
         rem = baki(num1,num2);
-        
+
+        //Display all values and results
         System.out.println(num1 + " + " + num2 + " = " + sum );
         System.out.println(num1 + " - " + num2 + " = " + sub );
         System.out.println(num1 + " x " + num2 + " = " + mul );
@@ -28,19 +30,19 @@ public class Arithmeticchap5 {
     }
 
 public static int add(int x, int y) {
-        return x + y; }
+        return x + y; } //ADDITION FORMULA
 
 public static int minus (int x, int y) {
-        return x - y; }
+        return x - y; } //SUBTRACTION FORMULA
 
 public static int darab(int x, int y) {
-        return x*y; }
+        return x*y; } //MULTIPLICATION FORMULA
 
 public static int bahagi(int x, int y) {
-        return x/y; }
+        return x/y; } //DIVISION FORMULA
 
 public static int baki(int x, int y) {
-        return x%y; }
+        return x%y; } //REMAINDER FORMULA
 
 
 }
